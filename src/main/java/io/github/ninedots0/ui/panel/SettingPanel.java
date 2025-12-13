de70da1f9de055351ec35fd0b82fe75f73f354f4
ninedots0 <2784914971@qq.com>
2025-12-14 00:47:45 +0800

    美化settingpanel页面

--- src/main/java/io/github/ninedots0/ui/panel/SettingPanel.java
+++ src/main/java/io/github/ninedots0/ui/panel/SettingPanel.java
@@ -1,9 +1,12 @@
 package io.github.ninedots0.ui.panel;
 
 import io.github.ninedots0.ui.frame.MainFrame;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import io.github.ninedots0.ui.util.AudioUtils;
 import io.github.ninedots0.ui.util.StyleUtils;
@@ -16,27 +19,42 @@ public class SettingPanel {
         this.mainFrame = frame;
     }
 
-    public VBox getContent() {
-        VBox root = new VBox(30);
-        root.setAlignment(Pos.CENTER);
+    public StackPane getContent() {
+
+        /* ===== 根布局 ===== */
+        StackPane root = new StackPane();
+        root.setPadding(new Insets(30));
+
+        /* ===== 中央设置区域 ===== */
+        VBox centerBox = new VBox(25);
+        centerBox.setAlignment(Pos.CENTER);
+
+        Label volumeLabel = new Label("音量");
+        volumeLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
 
         Slider volumeSlider = new Slider(0, 1, AudioUtils.getVolume());
+        volumeSlider.setPrefWidth(300);
         volumeSlider.setShowTickMarks(true);
         volumeSlider.setShowTickLabels(true);
         volumeSlider.setMajorTickUnit(0.25);
         volumeSlider.setBlockIncrement(0.1);
 
-        volumeSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
-            AudioUtils.setVolume(newVal.doubleValue());
-        });
+        volumeSlider.valueProperty().addListener((obs, oldVal, newVal) ->
+                AudioUtils.setVolume(newVal.doubleValue())
+        );
 
+        centerBox.getChildren().addAll(volumeLabel, volumeSlider);
+
+        /* ===== 左下角返回按钮 ===== */
         Button backBtn = new Button("返回主菜单");
-
         StyleUtils.applyMainMenuButton(backBtn);
-
         backBtn.setOnAction(e -> mainFrame.showMainMenu());
 
-        root.getChildren().addAll(backBtn, volumeSlider);
+        StackPane.setAlignment(backBtn, Pos.BOTTOM_LEFT);
+        StackPane.setMargin(backBtn, new Insets(0, 0, 10, 10));
+
+        /* ===== 组合 ===== */
+        root.getChildren().addAll(centerBox, backBtn);
         return root;
     }
 }
